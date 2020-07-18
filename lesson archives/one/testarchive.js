97b95b6d40e3faa06587b2e8fc20559428e86f0c
David Reke <david.e.reke@gmail.com>
2020-07-18 18:03:52 -0400

    getting ready to deployo to github

---
+++ lesson archives/one/testarchive.js
@@ -0,0 +1,14 @@
+
+
+
+// const name = 'David';
+
+// console.log(name);
+
+// 1. let's create a greet function
+const greet = (name) => {
+    console.log('hello' + name);
+}
+
+greet('David');
+greet('Melissa');
