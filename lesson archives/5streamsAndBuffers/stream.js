2884fdb19f3a39cc75cd72feb98d0aa54195060c
David Reke <david.e.reke@gmail.com>
2020-08-10 17:38:46 -0400

    changed stream.js

--- lesson archives/5streamsAndBuffers/stream.js
+++ lesson archives/5streamsAndBuffers/stream.js
@@ -4,30 +4,28 @@ const fs = require('fs')
 // read data by read streams and write data with write streams
 
 // 2. create a readStream. Ignore the second argument for now
-// const readStream = fs.createReadStream('./docs/blog3.txt')
-// part 5 continued.
-const readStream = fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'} )
-// first argument is where we want to write from
+const readStream = fs.createReadStream('./docs/blog3.txt')
 
-// 6. Now we are going to write a steam. This follow the format of read stream.
+// 3. use readStream.on
+// .on is an event listener for data
+readStream.on('data', (chunk) =>{
+    console.log('----- NEW CHUNK -----')
+    console.log(chunk)
+    // 4. Add .toSTring() to the chunk
+    //  ( console.log(chunk.toString()))
+    
+    // 5. remove the to.String() and ... 
+    // 5. add {encoding: 'utf8} line seven
+    //  (fs.createReadStream('./docs/blog3.txt', {encoding: 'utf8'}))
+    // 7.add a write stream divided up by a 'new chunk' stream
+    writeStream.write('\nNEW CHUNK\n')
+    writeStream.write(chunk);
+})
+
+// 6. create a write stream.
+
 const writeStream = fs.createWriteStream('./docs/blog4.txt')
 
-// 3. Create a readstream listener
-//  .on is an event listener that listens to data event.
-// every time we get a "data" event we take the chunk of data and fire callback function on that chunk of data
-// readStream.on('data', (chunk)=>{
-//     // add some console.logs.
-//     console.log('---- New Chunk ----')
-//     console.log(chunk)
-//     // 4.convert you console.log to console.log(chunk.toString())
-//     // console.log(chunk.toString())
-//     // 5 go back to createReadStream Method and add a second argument
-//     // 7. Add a write stream line in our readStream on callback function
-//     // The new chunk puts the words New Chunk in our blog.
-//     writeStream.write('\nNew Chunk\n')
-//     writeStream.write(chunk);
-// })
-
-
-// 8 now we're going to try piping, which is more simple but less flexible. It has to send data from a readable stream to a writeable stream.
-readStream.pipe(writeStream)
+// 8. lets add a pipe.
+// 8... make sure to delete blog 4 before running this
+readStream.pipe(writeStream);
